c65f3967ebbe860882149ff860fd66f7654d395a
dognellaf <dans-host@ya.ru>
2024-05-01 00:57:11 +0300

    new settings

--- calc/settings.py
+++ calc/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4b*y)2wek=3mgd9b7_%5z5wbl!m7q4wce_ppl@^0ftlq3_5+ek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
